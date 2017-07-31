f585159887195be25eddee3c3211c10c0bbb8c7c
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-07-31 16:50:06 -0700

    displaying the user name

--- assets/logic.js
+++ assets/logic.js
@@ -15,21 +15,16 @@
   var database = firebase.database();
 
   var playersRef = database.ref("/players");
-  var player1Ref;
-  var player2Ref;
 
   var playersNum = 0;
-  //var turn = 0;
+  
   var player = {name: "", wins: 0, losses: 0};
  
   var playersarr = [];
   var choices = ["Rock", "Paper", "Scissors"];
 
   var playersChoices = [];
-
-
-  //var player1Choice = "";
-  //var player2Choice = "";
+  var playersDatabaseNames = [];
 
 
 // When a child is added to the players folder in the database this function executes  
@@ -49,24 +44,24 @@ playersRef.on("child_added", function(snapshot, prevChildName){
           
 });
 
-playersRef.on("child_changed", function(snapshot){
+playersRef.on("child_changed", function(snapshot, prevChildName){
 
   console.log("child changed");
+  console.log(prevChildName);
   console.log(snapshot.val().choice);
+  var whoWon = 0;
   playersChoices.push(snapshot.val().choice);
+  playersDatabaseNames.push(snapshot.val().name);
   if (playersChoices.length === 2){
-      checkAnswer(playersChoices);
-
+      whoWon = checkAnswer(playersChoices);
+      var winnerName = playersDatabaseNames[whoWon-1];
+      console.log(winnerName);
+      $("#display-result").html(winnerName+" Wins!");
   }
 
-/*
-  if(snapshot.child("1").exists()){
 
-    console.log("player one exists");
-    var choice1 = snapshot.val();
-    console.log(choice1);
-  }
-  */
+
+
 });
 
 ///When the player hits start button this function executes
@@ -135,7 +130,6 @@ playersRef.on("child_changed", function(snapshot){
 
       }  
 
-
       displayChoices(whichPlayer);     
 
   }
@@ -154,52 +148,23 @@ playersRef.on("child_changed", function(snapshot){
 //the other player doesn't see the choice
 //the database is updated with the specific user's choice
 //a function is called to compare results
-    var clickedChoices = 0;
    $("li").click(function(){
 
 
         var userChoice = $(this).text();
-
-        clickedChoices++
-        
       
         $(this).parent().replaceWith("<p>"+userChoice+"</p>");
 
         var whichPlayer= $("#turn").data("value");
 
-
         playersRef.child((whichPlayer).toString()).update({choice: userChoice}); 
 
-/*
-        if(whichPlayer === 1)
-          player1Choice = userChoice;
-
-        else if(whichPlayer === 2) 
-          player2Choice = userChoice;
-*/
-
-  
-
         database.ref().update({turn: 2});
-
-        
-
-
-       
     });
-
-
-
-//refrencing the database of each player to get his choice
-
-
-
-
-
 
    ///compare players choices and determine wins and losses
    //update relative wins and losses for each player in the database
-
+   // Returns the number of the winner
    var checkAnswer = function(choicesArr){
 
         var wins1 = 0;
@@ -207,6 +172,7 @@ playersRef.on("child_changed", function(snapshot){
         var losses1 = 0;
         var losses2 = 0;
         var ties = 0;
+        var winner = 0;
         var choice1 = choicesArr[0];
         var choice2 = choicesArr[1];
 
@@ -215,25 +181,39 @@ playersRef.on("child_changed", function(snapshot){
         if ((choice1 === "Rock") && (choice2 === "Scissors")) {
 
           wins1++;
-         
-          losses2++
-          
+         losses2++
+         winner = 1; 
 
         } else if ((choice1 === "Rock") && (choice2 === "Paper")) {
+
           wins2++;
           losses1++;
+          winner = 2;
+
         } else if ((choice1 === "Scissors") && (choice2 === "Rock")) {
+
           wins2++;
           losses1++;
+          winner = 2;
+
         } else if ((choice1 === "Scissors") && (choice2 === "Paper")) {
+
            wins1++;
            losses2++;
+           winner = 1;
+
         } else if ((choice1 === "Paper") && (choice2 === "Rock")) {
+
            wins1++;
            losses2++;
+           winner = 1;
+
         } else if ((choice1 === "Paper") && (choice2 === "Scissors")) {
+
            wins2++;
            losses1++;
+           winner = 2;
+
         } else if (choice1 === choice2) {
           ties++;
         }
@@ -242,6 +222,7 @@ playersRef.on("child_changed", function(snapshot){
         playersRef.child("1").update({losses: losses1});
         playersRef.child("2").update({wins: wins2});
         playersRef.child("2").update({losses: losses2});
+        return winner;
 
    }
 
